e30bd6200312d11bfa9a25bc90d423c7dcef5545
khanhduy13 <duy01215624099@gmail.com>
2024-04-02 17:06:34 +0700

    ecommerce-BE

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+const UserRouter = require('../routes/UserRouter')
+const ProductRouter = require('../routes/ProductRouter')
+
+
+const routes = (app) => {
+   app.use('/api/user', UserRouter)
+   app.use('/api/product', ProductRouter)
+
+}
+
+module.exports = routes
